9ce7237b6d384c036108ed1ae1d912812b4029b6
Brad Bernard <bradbernard@me.com>
2017-01-24 16:50:58 -0800

    Added jobs and service provider validator extensions

---
+++ app/Jobs/InsertGameQuestionAnswers.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use Log;
+use App\Game;
+
+use App\Http\Controllers\Schedule\AnswersController;
+use App\Http\Controllers\Schedule\GradingController;
+
+class InsertGameQuestionAnswers implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $game;
+
+    public function __construct(Game $game)
+    {
+        $this->game = $game;
+    }
+
+    public function handle()
+    {
+        $answersController = new AnswersController();
+        $answersController->game = $this->game;
+        $answersController->insertAnswers();
+
+        $gradingController = new GradingController();
+        $gradingController->grade(); 
+    }
+}
