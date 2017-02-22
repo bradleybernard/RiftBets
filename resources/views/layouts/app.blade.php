0913e1e638cc3a75f2e00e373534ca2a01bed5db
Travis Takai <travistakai@gmail.com>
2017-02-22 14:12:38 -0800

    Moved navbar stuff from blade template to Navbar vue component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,36 +44,8 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-inverse navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        RiftBets
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        <li><a href="/schedule">Schedule</a></li>
-                        <li><a href="#">Bets</a></li>
-                        <li><a href="#">Leaderboard</a></li>
-                    </ul>
-                    <!-- Right Side Of Navbar -->
-                    <navbar></navbar>
-                </div>
-            </div>
-        </nav>
+        <!-- Navbar.vue Component -->
+        <navbar></navbar>
         @yield('content')
     </div>
     <script src="/js/app.js"></script>
