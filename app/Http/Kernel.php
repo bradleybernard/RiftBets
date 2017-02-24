64a413c79ba15e09ff794cc24ec28c5283fc1650
Brad Bernard <bradbernard@me.com>
2017-02-23 22:23:14 -0800

    Updated to add auth header if jwt token present

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \App\Http\Middleware\LoginIfJwtInCookies::class,
         ],
     ];
 
@@ -58,5 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'jwt' => \App\Http\Middleware\LoginIfJwtInCookies::class,
     ];
 }
