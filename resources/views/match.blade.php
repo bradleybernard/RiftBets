9fc84e37369e22fcf60ed106ce7338e20dd10890
Travis Takai <travistakai@gmail.com>
2017-03-02 10:31:27 -0800

    Added twitch stream

--- resources/views/match.blade.php
+++ resources/views/match.blade.php
@@ -4,5 +4,15 @@
 <div class="container">
     <!-- Testing matches -->
     <game-nav match="{{$matchId}}"></game-nav>
+
+	<iframe 
+	    src="http://player.twitch.tv/?channel=eulcs1" 
+	    height="720" 
+	    width="1280" 
+	    frameborder="0" 
+	    scrolling="no"
+	    allowfullscreen="true"
+	    style="max-width: 80em;">
+	</iframe>
 </div>
 @endsection
