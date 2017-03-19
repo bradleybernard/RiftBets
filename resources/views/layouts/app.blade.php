b5def890249716a68f36b66b1418de0f98f046a6
CinGer04 <ctiet0304@gmail.com>
2017-03-18 22:21:27 -0700

    color changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,12 @@
     <!-- Styles -->
     {{-- <link href="{{ mix('/css/app.css') }}" rel="stylesheet"> --}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<style>
+body{
+    background-color: #2b2b2d;
+    color: #fff;
+}
+</style>
 
     <!-- Scripts -->
     <script>
